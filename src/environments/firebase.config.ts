6cb71454f75ff52a827f398ea94325d00cd64c0e
Jany <my.maithi@gmail.com>
2017-10-29 22:51:31 +0800

    Delete firebase.config.ts

--- src/environments/firebase.config.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: 'AIzaSyALNQujJM1CakzfEE3wBLc6mzrYMP2w3CM',
-  authDomain: 'engagerocket-17c84.firebaseapp.com',
-  databaseURL: 'https://engagerocket-17c84.firebaseio.com',
-  projectId: 'engagerocket-17c84',
-  storageBucket: 'engagerocket-17c84.appspot.com',
-  messagingSenderId: '463953956355'
-};
